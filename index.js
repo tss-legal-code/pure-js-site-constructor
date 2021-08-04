229ea1c8b0bb9e305dabd096e28ea53f445dd38f
tss-legal-code <tss.legal.code@gmail.com>
2021-08-04 13:18:49 +0300

    functions implemented

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 
 const model = [
 	{type: "title", value:"Hello world!"},
-	{type: "text", value:"Here some text!"},
-	{type: "columns", value:["Here some 1 text!", "Here some 2 text!", "Here some 3 text!"]},
+	{type: "text", value:"Here some single text!"},
+	{type: "image", value:"./assets/gimp_render_filters_white.png"},
+	{type: "columns", value:["Here some 1st column text!", "Here some 2nd column text!", "Here some 3rd column text!"]},
 ];
 
 const $site = document.querySelector("#site");
@@ -14,38 +15,78 @@ model.forEach(block => {
 	let html = "";
 
 	if (block.type === "title") {
-		html = `
-		<div class="row">
-			<div class="col-sm">
-				<h1>${block.value}</h1>
-			</div>
-		</div>
-	`
+		html = title(block);
 	}else if (block.type === "text") {
-		html = `
-		<div class="row">
-			<div class="col-sm">
-				<p>${block.value}</p>
-			</div>
-		</div>
-		`
+		html = text(block);
 	}else if (block.type === "columns") {
-		html = `
-		<div class="row">
-                <div class="col-sm">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div class="col-sm">
-                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                </div>
-                <div class="col-sm">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-            </div>
-		`
+		html = columns(block);
+	}if (block.type === "image") {
+		html = image(block);
 	}
 
 	$site.insertAdjacentHTML("beforeend", html)
 
 });
 
+function title(block){
+	return `
+		<div class="row">
+			<div class="col-sm">
+				<h1>${block.value}</h1>
+			</div>
+		</div>
+		`
+}
+
+function text(block){
+	return `
+		<div class="row">
+			<div class="col-sm">
+				<p>${block.value}</p>
+			</div>
+		</div>
+		`
+}
+
+// NOT GOOD
+//    |
+//    V
+//
+// function columns(block){
+//
+// 	let html = "";
+//
+// 	block.value.forEach(element => {
+// 		html += `
+// 				 <div class="col-sm">
+//                     <p>${element}</p>
+//                 </div>
+// 		`
+// 	});
+// 	return `
+// 		<div class="row">
+//             ${html}
+//         </div>
+// 		`
+// }
+
+
+// better
+function columns(block){
+
+	const html = block.value.map( item => `<div class="col-sm">${item}</div>`);
+
+	return `
+		<div class="row">
+            ${html.join("")}
+        </div>
+		`
+}
+
+function image(block){
+	return `
+		<div class="row">
+			<img src="${block.value}"  alt="${block.value}" />
+		</div>
+		`
+}
